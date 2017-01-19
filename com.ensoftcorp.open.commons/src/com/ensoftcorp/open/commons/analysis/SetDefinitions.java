1aecc42052c61c34130fdfb67d06bed66b883917
Ben Holland <benjholla@gmail.com>
2017-01-19 16:11:19 -0600

    bug fix for set definitions

--- com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/analysis/SetDefinitions.java
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/analysis/SetDefinitions.java
@@ -15,10 +15,6 @@ import com.ensoftcorp.atlas.core.xcsg.XCSG;
 public final class SetDefinitions {
 
 private SetDefinitions() {}
-	
-	private static Q index() {
-		return Common.codemap();
-	}
 
 	/**
 	 * Types which represent arrays of other types
@@ -27,7 +23,7 @@ private SetDefinitions() {}
 	 * project.
 	 */
 	public static Q arrayTypes() {
-		return index().nodesTaggedWithAny(XCSG.ArrayType);
+		return Common.universe().nodesTaggedWithAny(XCSG.ArrayType);
 	}
 
 	/**
@@ -37,69 +33,23 @@ private SetDefinitions() {}
 	 * project.
 	 */
 	public static Q primitiveTypes() {
-		return index().nodesTaggedWithAny(XCSG.Primitive);
-	}
-
-	/**
-	 * Summary invoke nodes, representing invocations on methods.
-	 * 
-	 * NOTE: These nodes are NOT declared by anything. They are outside of any
-	 * project.
-	 */
-	public static Q invokeNodes() {
-		return index().nodesTaggedWithAny(Node.INVOKE);
+		return Common.universe().nodesTaggedWithAny(XCSG.Primitive);
 	}
 
 	/**
 	 * Everything declared under any of the known API projects, if they are in
 	 * the index.
 	 */
-	public static Q apis() {
-		return CommonQueries.declarations(index().nodesTaggedWithAny(XCSG.Library), TraversalDirection.FORWARD).difference(arrayTypes(),
-				primitiveTypes(), invokeNodes());
-	}
-
-	/**
-	 * Methods defined in java.lang.Object, and all methods which override them
-	 */
-	public static Q objectMethodOverrides() {
-		return Common.edges(XCSG.Overrides).reverse(
-				CommonQueries.declarations(Common.typeSelect("java.lang", "Object"), TraversalDirection.FORWARD).nodesTaggedWithAny(XCSG.Method));
+	public static Q libraries() {
+		return Common.universe().nodesTaggedWithAny(XCSG.Library).contained();
 	}
 
 	/**
 	 * Everything in the universe which is part of the app (not part of the
-	 * apis, or any "floating" nodes).
+	 * libraries, or any "floating" nodes).
 	 */
 	public static Q app() {
-		return index().difference(apis(), invokeNodes(), arrayTypes(), primitiveTypes());
+		return Common.universe().difference(libraries(), primitiveTypes(), arrayTypes());
 	}
 
-	/**
-	 * All method nodes declared by the APIs.
-	 */
-	public static Q apiMethods() {
-		return apis().nodesTaggedWithAny(XCSG.Method);
-	}
-
-	/**
-	 * All variable nodes declared by the APIs.
-	 */
-	public static Q apiVariables() {
-		return apis().nodesTaggedWithAny(XCSG.Variable);
-	}
-
-	/**
-	 * All data flow nodes declared by the APIs.
-	 */
-	public static Q apiDFN() {
-		return apis().nodesTaggedWithAny(XCSG.DataFlow_Node);
-	}
-
-	/**
-	 * All edges for which both endpoints lay within the APIs.
-	 */
-	public static Q apiEdges() {
-		return apis().induce(index());
-	}
 }
