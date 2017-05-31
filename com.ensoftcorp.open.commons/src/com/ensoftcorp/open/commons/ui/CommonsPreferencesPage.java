b64d5ff4626709c3a1e64fdf5e98eeef8a95d9d7
Ben Holland <benjholla@gmail.com>
2017-05-31 17:18:05 -0500

    updated description

--- com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/ui/CommonsPreferencesPage.java
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/ui/CommonsPreferencesPage.java
@@ -20,7 +20,7 @@ public class CommonsPreferencesPage extends FieldEditorPreferencePage implements
 	private static final String COMPUTE_CONTROL_FLOW_GRAPH_DOMINANCE_TREES_DESCRIPTION = "Compute control flow graph dominance trees";
 	private static final String COMPUTE_EXCEPTIONAL_CONTROL_FLOW_GRAPH_DOMINANCE_TREES_DESCRIPTION = "Compute exceptional control flow graph dominance trees";
 	private static final String ADD_MASTER_ENTRY_EXIT_CONTAINMENT_RELATIONSHIPS_DESCRIPTION = "Add master entry/exit containment relationships";
-	private static final String DISPLAY_FILTER_VIEW_RESULT_CONTAINERS_DESCRIPTION = "Enables displaying the container relationships in filter view results";
+	private static final String DISPLAY_FILTER_VIEW_RESULT_CONTAINERS_DESCRIPTION = "Display container relationships in filter view results";
 	
 	private static boolean changeListenerAdded = false;
 	
