232b85e96a4b6d39370ef181419925cfcc9022a3
Ben Holland <benjholla@gmail.com>
2017-07-23 15:35:46 -0500

    added an index change listener

---
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/utilities/CodeMapChangeListener.java
@@ -0,0 +1,40 @@
+package com.ensoftcorp.open.commons.utilities;
+
+import com.ensoftcorp.atlas.core.indexing.IIndexListener;
+
+public class CodeMapChangeListener implements IIndexListener {
+
+	private boolean indexHasChanged = false;
+	private boolean indexIsChanging = false;
+	
+	public boolean hasIndexChanged(){
+		return indexHasChanged;
+	}
+	
+	public void reset(){
+		indexHasChanged = false;
+	}
+	
+	@Override
+	public void indexOperationCancelled(IndexOperation io) {
+		indexHasChanged = true;
+	}
+
+	@Override
+	public void indexOperationError(IndexOperation io, Throwable t) {
+		indexHasChanged = true;
+	}
+
+	@Override
+	public void indexOperationStarted(IndexOperation io) {
+		indexHasChanged = true;
+	}
+
+	@Override
+	public void indexOperationComplete(IndexOperation io) {
+		indexHasChanged = true;
+	}
+
+	@Override
+	public void indexOperationScheduled(IndexOperation io) {}
+};
