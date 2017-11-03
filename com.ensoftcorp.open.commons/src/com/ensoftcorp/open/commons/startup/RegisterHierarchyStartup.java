523248b1d6584e3b8a0ebd08d09459c04159c01e
benjholla <benjholla@gmail.com>
2017-11-03 13:25:51 -0500

    fix for subsystem tag hierarchy missing on index load after Atlas restart

--- com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/startup/RegisterHierarchyStartup.java
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/startup/RegisterHierarchyStartup.java
@@ -1,12 +1,91 @@
 package com.ensoftcorp.open.commons.startup;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
 import org.eclipse.ui.IStartup;
+
+import com.ensoftcorp.atlas.core.xcsg.XCSG;
+import com.ensoftcorp.open.commons.log.Log;
+import com.ensoftcorp.open.commons.subsystems.Subsystem;
+import com.ensoftcorp.open.commons.subsystems.Subsystems;
 
 public class RegisterHierarchyStartup implements IStartup {
 
 	@Override
 	public void earlyStartup() {
-		// TODO: implement
+		try {
+			Subsystems.loadSubsystemContributions();
+			Set<Subsystem> subsystems = Subsystems.getRegisteredSubsystems();
+			buildSubsystemTagHierarchy(subsystems);
+		} catch (Exception e){
+			Log.error("Unable to build subsystem tag hierarchy.", e);
+		}
 	}
+	
+	private void buildSubsystemTagHierarchy(Set<Subsystem> subsystems) {
+		// register root tag
+		try {
+			XCSG.HIERARCHY.registerTag(Subsystem.ROOT_SUBSYSTEM_TAG, new String[] {});
+		} catch (IllegalStateException e){
+			// its ok if the tag was already registered, then we are
+			// successful for the purposes of building the hierarchy
+			if(!e.getMessage().contains("Tag is already registered!")){
+				throw e;
+			}
+		}
 
+		// create a worklist of subsystems to register
+		LinkedList<Subsystem> registrationWorklist = new LinkedList<Subsystem>();
+		
+		for(Subsystem subsystem : subsystems){
+			registrationWorklist.add(subsystem);
+		}
+		
+		// get the registered tags
+		HashSet<String> registeredTags = new HashSet<String>();
+		for (String registeredTag : XCSG.HIERARCHY.registeredTags()) {
+			registeredTags.add(registeredTag);
+		}
+		
+		// register each tag adding the parents
+		// parents tags must be registered first
+		while (!registrationWorklist.isEmpty()) {
+			// for each subsystem to register
+			LinkedList<Subsystem> completedRegistrations = new LinkedList<Subsystem>();
+			for (Subsystem subsystem : registrationWorklist) {
+				// get the subsystems parent tags
+				HashSet<String> parents = new HashSet<String>();
+				for (String parent : subsystem.getParentTags()) {
+					parents.add(parent);
+				}
+				// check if all of the parent tags in the subsystem have been registered
+				if (registeredTags.containsAll(parents)) {
+					// register the subsystem
+					try {
+						XCSG.HIERARCHY.registerTag(subsystem.getTag(), subsystem.getParentTags());
+					} catch (IllegalStateException e){
+						// its ok if the tag was already registered, then we are
+						// successful for the purposes of building the hierarchy
+						if(!e.getMessage().contains("Tag is already registered!")){
+							throw e;
+						}
+					}
+					registeredTags.add(subsystem.getTag());
+					completedRegistrations.add(subsystem);
+				}
+			}
+			// if we couldn't register any subsystems and the worklist is not empty,
+			// then we must have a cycle in the hierarchy :(
+			if (!registrationWorklist.removeAll(completedRegistrations)) {
+				Log.warning("An issue was detected in the subsystem hierarchy.\n"
+						+ "There may be a dependency cycle or required parent subsystems may not have been contributed properly and are missing."
+						+ "The following subsystems were not registered successfully.\n"
+						+ registrationWorklist.toString());
+				break;
+			}
+		}
+	}
+	
 }
--- com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/subsystems/SubsystemCodemapStage.java
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/subsystems/SubsystemCodemapStage.java
@@ -1,12 +1,9 @@
 package com.ensoftcorp.open.commons.subsystems;
 
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 
-import com.ensoftcorp.atlas.core.xcsg.XCSG;
 import com.ensoftcorp.open.commons.codemap.PrioritizedCodemapStage;
 import com.ensoftcorp.open.commons.log.Log;
 import com.ensoftcorp.open.commons.preferences.SubsystemPreferences;
@@ -54,8 +51,6 @@ public class SubsystemCodemapStage extends PrioritizedCodemapStage {
 		}
 		
 		if(hasEnabledSubystemCategory){
-			buildSubsystemTagHierarchy(subsystems);
-			
 			for (Subsystem subsystem : subsystems) {
 				if (monitor.isCanceled()) {
 					break;
@@ -64,75 +59,6 @@ public class SubsystemCodemapStage extends PrioritizedCodemapStage {
 						subsystem.tagSubsystem();
 					}
 				}
-			}
-		}
-	}
-	
-	private void buildSubsystemTagHierarchy(Set<Subsystem> subsystems) {
-		
-		// TODO: migrate this to the RegistryHierarchyStartup.earlyStartup method
-		
-		// register root tag
-		try {
-			XCSG.HIERARCHY.registerTag(Subsystem.ROOT_SUBSYSTEM_TAG, new String[] {});
-		} catch (IllegalStateException e){
-			// its ok if the tag was already registered, then we are
-			// successful for the purposes of building the hierarchy
-			if(!e.getMessage().contains("Tag is already registered!")){
-				throw e;
-			}
-		}
-
-		// create a worklist of subsystems to register
-		LinkedList<Subsystem> registrationWorklist = new LinkedList<Subsystem>();
-		
-		for(Subsystem subsystem : subsystems){
-			if(SubsystemPreferences.isSubsystemCategoryEnabled(subsystem.getCategory())){
-				registrationWorklist.add(subsystem);
-			}
-		}
-		
-		// get the registered tags
-		HashSet<String> registeredTags = new HashSet<String>();
-		for (String registeredTag : XCSG.HIERARCHY.registeredTags()) {
-			registeredTags.add(registeredTag);
-		}
-		
-		// register each tag adding the parents
-		// parents tags must be registered first
-		while (!registrationWorklist.isEmpty()) {
-			// for each subsystem to register
-			LinkedList<Subsystem> completedRegistrations = new LinkedList<Subsystem>();
-			for (Subsystem subsystem : registrationWorklist) {
-				// get the subsystems parent tags
-				HashSet<String> parents = new HashSet<String>();
-				for (String parent : subsystem.getParentTags()) {
-					parents.add(parent);
-				}
-				// check if all of the parent tags in the subsystem have been registered
-				if (registeredTags.containsAll(parents)) {
-					// register the subsystem
-					try {
-						XCSG.HIERARCHY.registerTag(subsystem.getTag(), subsystem.getParentTags());
-					} catch (IllegalStateException e){
-						// its ok if the tag was already registered, then we are
-						// successful for the purposes of building the hierarchy
-						if(!e.getMessage().contains("Tag is already registered!")){
-							throw e;
-						}
-					}
-					registeredTags.add(subsystem.getTag());
-					completedRegistrations.add(subsystem);
-				}
-			}
-			// if we couldn't register any subsystems and the worklist is not empty,
-			// then we must have a cycle in the hierarchy :(
-			if (!registrationWorklist.removeAll(completedRegistrations)) {
-				Log.warning("An issue was detected in the subsystem hierarchy.\n"
-						+ "There may be a dependency cycle or required parent subsystems may not have been contributed properly and are missing."
-						+ "The following subsystems were not registered successfully.\n"
-						+ registrationWorklist.toString());
-				break;
 			}
 		}
 	}
