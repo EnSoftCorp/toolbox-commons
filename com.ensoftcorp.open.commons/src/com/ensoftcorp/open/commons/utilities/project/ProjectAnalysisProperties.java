97d9f028b245f861b898a848c489166cfd565c6a
Ben Holland <benjholla@gmail.com>
2017-12-28 16:05:17 -0600

    a few paranoid guards

--- com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/utilities/project/ProjectAnalysisProperties.java
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/utilities/project/ProjectAnalysisProperties.java
@@ -32,57 +32,63 @@ public class ProjectAnalysisProperties {
 	public static final String ROOT_ELEMENT = "properties";
 	
 	private static void initializeDefaultProjectProperties(IProject project) throws Exception {
-		Log.info("Initializing default analysis project properties for " + project.getName() + ".");
-		AnalysisPropertiesInitializers.loadAnalysisPropertiesInitializerContributions();
-		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		Document properties = builder.newDocument();
-		
-		Element rootElement = properties.createElement(ROOT_ELEMENT);
-		properties.appendChild(rootElement);
-		
-		for(AnalysisPropertiesInitializer initializer : AnalysisPropertiesInitializers.getRegisteredAnalysisPropertiesInitializers()){
-			if(initializer.supportsProject(project)){
-				try {
-					initializer.initialize(project, properties);
-				} catch (Exception e){
-					Log.error("Error initializing properties", e);
+		synchronized (ProjectAnalysisProperties.class){
+			Log.info("Initializing default analysis project properties for " + project.getName() + ".");
+			AnalysisPropertiesInitializers.loadAnalysisPropertiesInitializerContributions();
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			Document properties = builder.newDocument();
+			
+			Element rootElement = properties.createElement(ROOT_ELEMENT);
+			properties.appendChild(rootElement);
+			
+			for(AnalysisPropertiesInitializer initializer : AnalysisPropertiesInitializers.getRegisteredAnalysisPropertiesInitializers()){
+				if(initializer.supportsProject(project)){
+					try {
+						initializer.initialize(project, properties);
+					} catch (Exception e){
+						Log.error("Error initializing properties", e);
+					}
 				}
 			}
-		}
-		setAnalysisProperties(project, properties);
-		try {
-			project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
-		} catch (Throwable t) {
-			// just a best effort, we don't really care if the UI is updated
+			setAnalysisProperties(project, properties);
+			try {
+				project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+			} catch (Throwable t) {
+				// just a best effort, we don't really care if the UI is updated
+			}
 		}
 	}
 	
 	public static Document getAnalysisProperties(IProject project) throws Exception {
-		File propertiesFile = new File(project.getFile(PROPERTIES_PATH).getLocation().toOSString());
-		if(!propertiesFile.exists()){
-			initializeDefaultProjectProperties(project);
+		synchronized (ProjectAnalysisProperties.class){
+			File propertiesFile = new File(project.getFile(PROPERTIES_PATH).getLocation().toOSString());
+			if(!propertiesFile.exists()){
+				initializeDefaultProjectProperties(project);
+			}
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			return builder.parse(new FileInputStream(propertiesFile));
 		}
-		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		return builder.parse(new FileInputStream(propertiesFile));
 	}
 	
 	public static void setAnalysisProperties(IProject project, Document properties) throws Exception {
-		// remove blank lines
-		// reference: https://stackoverflow.com/a/12670194/475329
-		XPath xp = XPathFactory.newInstance().newXPath();
-		NodeList nl = (NodeList) xp.evaluate("//text()[normalize-space(.)='']", properties, XPathConstants.NODESET);
-		for (int i=0; i < nl.getLength(); ++i) {
-		    Node node = nl.item(i);
-		    node.getParentNode().removeChild(node);
+		synchronized (ProjectAnalysisProperties.class){
+			// remove blank lines
+			// reference: https://stackoverflow.com/a/12670194/475329
+			XPath xp = XPathFactory.newInstance().newXPath();
+			NodeList nl = (NodeList) xp.evaluate("//text()[normalize-space(.)='']", properties, XPathConstants.NODESET);
+			for (int i=0; i < nl.getLength(); ++i) {
+			    Node node = nl.item(i);
+			    node.getParentNode().removeChild(node);
+			}
+			// save the indented xml file
+			File propertiesFile = new File(project.getFile(PROPERTIES_PATH).getLocation().toOSString());
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "3");
+			transformer.transform(new DOMSource(properties), new StreamResult(new FileOutputStream(propertiesFile)));
 		}
-		// save the indented xml file
-		File propertiesFile = new File(project.getFile(PROPERTIES_PATH).getLocation().toOSString());
-		Transformer transformer = TransformerFactory.newInstance().newTransformer();
-		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-		transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-		transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "3");
-		transformer.transform(new DOMSource(properties), new StreamResult(new FileOutputStream(propertiesFile)));
 	}
 	
 }
