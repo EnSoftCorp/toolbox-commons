ff17f4734f84fa22b9924f4792430c9334675833
benjholla <benjholla@gmail.com>
2018-05-11 21:09:05 -0500

    bug fix for when source correspondence is null

--- com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/utilities/NodeSourceCorrespondenceSorter.java
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/utilities/NodeSourceCorrespondenceSorter.java
@@ -12,6 +12,33 @@ public class NodeSourceCorrespondenceSorter implements Comparator<Node> {
 	public int compare(Node n1, Node n2) {
 		SourceCorrespondence sc1 = (SourceCorrespondence) n1.getAttr(XCSG.sourceCorrespondence);
 		SourceCorrespondence sc2 = (SourceCorrespondence) n2.getAttr(XCSG.sourceCorrespondence);
+		
+		// treat null source correspondence as bigger than non-null source correspondence
+		if(sc1 == null || sc2 == null) {
+			if(sc1 == null && sc2 == null) {
+				return 0;
+			}
+			if(sc1 != null && sc2 == null) {
+				return -1;
+			}
+			if(sc1 == null && sc2 != null) {
+				return 1;
+			}
+		}
+		
+		// treat null source correspondence files as bigger than non-null source correspondence files
+		if(sc1.sourceFile == null || sc2.sourceFile == null) {
+			if(sc1.sourceFile == null && sc2.sourceFile == null) {
+				return 0;
+			}
+			if(sc1.sourceFile != null && sc2.sourceFile == null) {
+				return -1;
+			}
+			if(sc1.sourceFile == null && sc2.sourceFile != null) {
+				return 1;
+			}
+		}
+		
 		String path1 = sc1.sourceFile.getLocation().toOSString();
 		String path2 = sc2.sourceFile.getLocation().toOSString();
 		int fileComparison = path1.compareTo(path2);
