9cd67549ab5a2f9559df18a91bccf4fd2cc018fc
Jon Mathews <jon@ensoftcorp.com>
2018-09-10 15:34:20 -0500

    Refactor ##signature to common constant

---
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/xcsg/Undocumented.java
@@ -0,0 +1,17 @@
+package com.ensoftcorp.open.commons.xcsg;
+
+/**
+ * Undocumented/internal XCSG schema implemented by Atlas.
+ * Use at your own risk.
+ * 
+ */
+public class Undocumented {
+	/**
+	 * An undocumented, but very useful Atlas attribute.
+	 * 
+	 * This attribute key corresponds to the raw Java/Jimple function signature as a string,
+	 * which includes the return type, function name, and parameters.
+	 */
+	public static final String SIGNATURE = "##signature"; //$NON-NLS-1$
+
+}
