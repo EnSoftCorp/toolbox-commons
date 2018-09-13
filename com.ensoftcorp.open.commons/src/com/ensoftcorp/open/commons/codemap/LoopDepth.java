d792b6248f984c52333746a322efca4ae670288a
Jon Mathews <jon@ensoftcorp.com>
2018-09-13 18:09:26 -0500

    Calculate loop depth for languages other than Jimple; use for Markup

---
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/codemap/LoopDepth.java
@@ -0,0 +1,60 @@
+package com.ensoftcorp.open.commons.codemap;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import com.ensoftcorp.atlas.core.db.graph.Edge;
+import com.ensoftcorp.atlas.core.db.graph.Node;
+import com.ensoftcorp.atlas.core.db.set.AtlasNodeHashSet;
+import com.ensoftcorp.atlas.core.db.set.AtlasSet;
+import com.ensoftcorp.atlas.core.query.Q;
+import com.ensoftcorp.atlas.core.query.Query;
+import com.ensoftcorp.atlas.core.xcsg.XCSG;
+import com.ensoftcorp.open.commons.xcsg.Toolbox;
+
+public class LoopDepth extends PrioritizedCodemapStage {
+	/**
+	 * The unique identifier for the codemap stage
+	 */
+	public static final String IDENTIFIER = "com.ensoftcorp.open.commons.loopdepth"; //$NON-NLS-1$
+
+	@Override
+	public String getDisplayName() {
+		return "Computing Loop Depth"; //$NON-NLS-1$
+	}
+
+	@Override
+	public String getIdentifier() {
+		return IDENTIFIER;
+	}
+
+	@Override
+	public String[] getCodemapStageDependencies() {
+		return new String[]{};
+	}
+
+	@Override
+	public void performIndexing(IProgressMonitor monitor) {
+		Q loopHeaders = Query.universe().nodes(XCSG.Loop).nodes(XCSG.Language.C, XCSG.Language.CPP, XCSG.Language.Java).difference(Query.universe().nodes(XCSG.Language.Jimple));
+		Q edges = Query.universe().edges(XCSG.LoopChild);
+		Q loops = loopHeaders.reverseStepOn(edges);
+		Q qlevel1LoopHeaders = loops.roots();
+		AtlasSet<Node> level1LoopHeaders = new AtlasNodeHashSet(qlevel1LoopHeaders.eval().nodes());
+		for (Node loopHeader : level1LoopHeaders) {
+			recordLoopDepth(loopHeader, 1);
+		}
+	}
+
+	private void recordLoopDepth(Node loopHeader, int depth) {
+		loopHeader.putAttr(Toolbox.loopDepth, depth);
+		AtlasSet<Edge> loopChildren = loopHeader.out(XCSG.LoopChild);
+		for (Edge loopChild : loopChildren) {
+			Node member = loopChild.to();
+			if (member.taggedWith(XCSG.Loop)) {
+				recordLoopDepth(member, depth+1);
+			} else {
+				member.putAttr(Toolbox.loopDepth, depth);
+			}
+		}
+	}
+
+}
--- com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/highlighter/LoopHighlighter.java
+++ com.ensoftcorp.open.commons/src/com/ensoftcorp/open/commons/highlighter/LoopHighlighter.java
@@ -1,23 +1,30 @@
 package com.ensoftcorp.open.commons.highlighter;
 
 import java.awt.Color;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.ensoftcorp.atlas.core.db.graph.Node;
-import com.ensoftcorp.atlas.core.db.set.AtlasSet;
 import com.ensoftcorp.atlas.core.markup.Markup;
 import com.ensoftcorp.atlas.core.markup.MarkupProperty;
 import com.ensoftcorp.atlas.core.query.Q;
-import com.ensoftcorp.atlas.core.script.Common;
-import com.ensoftcorp.atlas.core.xcsg.XCSG;
+import com.ensoftcorp.atlas.core.query.Query;
+import com.ensoftcorp.open.commons.xcsg.Toolbox;
 
 public class LoopHighlighter {
 
 	/** 
 	 * The default blue fill color for CFG nodes
 	 */
-	public static final Color cfgNodeFillColor = new Color(51, 175, 243).darker();
+	private static final Color cfgNodeFillColor = new Color(51, 175, 243);
+	private static List<Color> depthToColor = new ArrayList<>();
+	static {
+		Color color = cfgNodeFillColor;
+		int MAX_DEPTH = 11;
+		for (int i=0; i<MAX_DEPTH; i++) {
+			depthToColor.add(color);
+			color = color.darker();
+		}
+	}
 
 	/**
 	 * Adds markup for loops and loop children. Nodes are colored a darker color
@@ -28,39 +35,11 @@ public class LoopHighlighter {
 	 * @return
 	 */
 	public static Markup applyHighlightsForLoopDepth(Markup m) {
-		Q loopChildEdges =  Common.universe().edges(XCSG.LoopChild);
-		Q loopHeadersQ = Common.universe().nodes(XCSG.Loop);
-		AtlasSet<Node> loopHeaders = loopHeadersQ.eval().nodes();
-		
-		Map<Node, Color> colorMap = new HashMap<Node, Color>();
-		for (Node loopHeader : loopHeaders) {
-			Color color = applyHighlightsForLoopDepth(colorMap, loopHeader);
-			m.set(loopHeader, MarkupProperty.NODE_BACKGROUND_COLOR, color);
-		}
-		
-		// set color of loop members (other than loop headers) to same color as header 
-		Q loopChildren = loopChildEdges.successors(loopHeadersQ);
-		for(Node child : loopChildren.eval().nodes()) {
-			Node loopHeader = loopChildEdges.predecessors(Common.toQ(child)).eval().nodes().one();
-			m.set(child, MarkupProperty.NODE_BACKGROUND_COLOR, colorMap.get(loopHeader));
+		for (int i=1; i<depthToColor.size(); i++) {
+			Q depth = Query.universe().selectNode(Toolbox.loopDepth, i);
+			m.setNode(depth, MarkupProperty.NODE_BACKGROUND_COLOR, depthToColor.get(i));
 		}
 		return m;
 	}
 
-	private static Color applyHighlightsForLoopDepth(Map<Node, Color> colorMap, Node loopHeader) {
-		Q loopChildEdges =  Common.universe().edges(XCSG.LoopChild);
-		Color color = colorMap.get(loopHeader);
-		if (color == null) {
-			Node parentLoopHeader = loopChildEdges.predecessors(Common.toQ(loopHeader)).eval().nodes().one();
-			if (parentLoopHeader == null) {
-				// loop is not nested
-				color = cfgNodeFillColor;
-			} else {
-				color = applyHighlightsForLoopDepth(colorMap, parentLoopHeader);
-				color = color.darker();
-			}
-			colorMap.put(loopHeader, color);
-		}
-		return color;
-	}
 }
